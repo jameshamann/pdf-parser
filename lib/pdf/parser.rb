9c5724a136e928b5da3b6212f382fdc199b2c9c2
James Hamann <jameshamann@me.com>
2018-05-01 17:00:33 +0100

    adds parse function to parser.rb

--- lib/pdf/parser.rb
+++ lib/pdf/parser.rb
@@ -1,9 +1,25 @@
 require "pdf/parser/version"
-require 'dotenv'
-Dotenv.load
+require "nokogiri"
+require "origami"
+
+include Origami
+
+class String
+  def string_between_markers marker1, marker2
+    self[/#{Regexp.escape(marker1)}(.*?)#{Regexp.escape(marker2)}/m, 1]
+  end
+end
 
 module Pdf
   module Parser
-    # Your code goes here...
+    def parse(pdf)
+      parsed = PDF.read(pdf)
+      data = parsed.grep("><xfa:data")
+      string = data.to_s
+      val = string.string_between_markers('<xfa:data\n>', '</Form\n>')
+      final = val.gsub('\n','')
+      xml = Nokogiri::XML(final).to_xml
+      doc = Nokogiri::XML(xml)
+    end
   end
 end
